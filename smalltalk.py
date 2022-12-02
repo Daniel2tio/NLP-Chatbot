a243d7cdf44e90885cef0842277d8c8142696632
Daniel Abreu <danielabreu1738@gmail.com>
2022-12-02 15:47:34 +0000

    Small Talk function

--- smalltalk.py
+++ smalltalk.py
@@ -1,6 +1,26 @@
-
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics import pairwise_distances
 
 
 #dataset of common small talk questions
 
 dataset = './Data/Small Talk.csv'
+
+#function to repond with small talk answers based on user questions
+
+def smalltalk_answers(query, threshold):
+
+    df = pd.read_csv(dataset)
+
+    #TF-IDF
+    tfidf_vec = TfidfVectorizer(analyzer='word')
+    X_tfidf = tfidf_vec.fit_transform(df['Question']).toarray()
+    df_tfidf = pd.DataFrame(X_tfidf, columns = tfidf_vec.get_feature_names.out())
+
+    # process query 
+    input_tfidf = tfidf_vec.transform([query.lower()]).toarray()
+
+    # cosine similarity
+    cos = 1 - pairwise_distances(df_tfidf, input_tfidf, metric = 'cosine')
+    
